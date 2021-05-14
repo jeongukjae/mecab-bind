9739600f65c14bf8d76fc5ac192f2a9a06b4e675
Jeong Ukjae <JeongUkJae@gmail.com>
2021-05-15 00:57:39 +0900

    update python/move_ops.sh

--- python/move_ops.sh
+++ python/move_ops.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 RUNFILES_DIR=$(pwd)
-cp -p "${RUNFILES_DIR}/python/mecab/bind.so" "${BUILD_WORKSPACE_DIRECTORY}/python/mecab/bind.so"
+cp -pf "${RUNFILES_DIR}/python/mecab/bind.so" "${BUILD_WORKSPACE_DIRECTORY}/python/mecab/bind.so"
