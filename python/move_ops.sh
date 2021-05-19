d0d92e3189b1643e3e698392917f57bb848a88b6
Jeong Ukjae <JeongUkJae@gmail.com>
2021-05-19 18:28:43 +0900

    update move_ops and gitignore

--- python/move_ops.sh
+++ python/move_ops.sh
@@ -4,4 +4,5 @@ RUNFILES_DIR=$(pwd)
 
 cp -pf "${RUNFILES_DIR}/python/mecab/bind.so" "${BUILD_WORKSPACE_DIRECTORY}/python/mecab/bind.so"
 cp -pf ${BUILD_WORKSPACE_DIRECTORY}/bazel-bin/libmecab/lib/* "${BUILD_WORKSPACE_DIRECTORY}/python/mecab/"
+mkdir -p "${BUILD_WORKSPACE_DIRECTORY}/python/mecab/include/"
 cp -pf ${BUILD_WORKSPACE_DIRECTORY}/bazel-bin/libmecab/include/* "${BUILD_WORKSPACE_DIRECTORY}/python/mecab/include/"
