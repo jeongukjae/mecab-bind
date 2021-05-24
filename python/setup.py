008ce1c71a2bc6232205a6fa3d5cbd26f895030f
Jeong Ukjae <JeongUkJae@gmail.com>
2021-05-25 00:12:32 +0900

    update publish action

--- python/setup.py
+++ python/setup.py
@@ -23,7 +23,7 @@ with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'README.md'),
 
 setup(
     name="mecab-bind",
-    version="0.996.0a3",
+    version="0.996.0a4",
     python_requires=">=3.6",
     packages=find_packages(),
     entry_points = {
